739d7ec15143db9fc01066d503de2cf1348ee676
Santiago Videla <santiago.videla@gmail.com>
2020-06-01 01:27:02 -0300

    drop python 2.x and fix lowercase_ext

--- flask_uploads.py
+++ flask_uploads.py
@@ -9,79 +9,73 @@ an `UploadSet` object and upload your files to it.
 :license:   MIT/X11, see LICENSE for details
 """
 
-import sys
-
-PY3 = sys.version_info[0] == 3
-
-if PY3:
-    string_types = str,
-else:
-    string_types = basestring,
-
-import os.path
+import os
 import posixpath
+import itertools
 
 from flask import current_app, send_from_directory, abort, url_for
-from itertools import chain
+from flask import Blueprint
 from werkzeug.datastructures import FileStorage
 from werkzeug.utils import secure_filename
 
-from flask import Blueprint
 
 # Extension presets
 
 #: This just contains plain text files (.txt).
-TEXT = ('txt',)
+TEXT = ("txt",)
 
 #: This contains various office document formats (.rtf, .odf, .ods, .gnumeric,
 #: .abw, .doc, .docx, .xls, .xlsx and .pdf). Note that the macro-enabled versions
 #: of Microsoft Office 2007 files are not included.
-DOCUMENTS = tuple('rtf odf ods gnumeric abw doc docx xls xlsx pdf'.split())
+DOCUMENTS = tuple("rtf odf ods gnumeric abw doc docx xls xlsx pdf".split())
 
 #: This contains basic image types that are viewable from most browsers (.jpg,
 #: .jpe, .jpeg, .png, .gif, .svg, .bmp and .webp).
-IMAGES = tuple('jpg jpe jpeg png gif svg bmp webp'.split())
+IMAGES = tuple("jpg jpe jpeg png gif svg bmp webp".split())
 
 #: This contains audio file types (.wav, .mp3, .aac, .ogg, .oga, and .flac).
-AUDIO = tuple('wav mp3 aac ogg oga flac'.split())
+AUDIO = tuple("wav mp3 aac ogg oga flac".split())
 
 #: This is for structured data files (.csv, .ini, .json, .plist, .xml, .yaml,
 #: and .yml).
-DATA = tuple('csv ini json plist xml yaml yml'.split())
+DATA = tuple("csv ini json plist xml yaml yml".split())
 
 #: This contains various types of scripts (.js, .php, .pl, .py .rb, and .sh).
 #: If your Web server has PHP installed and set to auto-run, you might want to
 #: add ``php`` to the DENY setting.
-SCRIPTS = tuple('js php pl py rb sh'.split())
+SCRIPTS = tuple("js php pl py rb sh".split())
 
 #: This contains archive and compression formats (.gz, .bz2, .zip, .tar,
 #: .tgz, .txz, and .7z).
-ARCHIVES = tuple('gz bz2 zip tar tgz txz 7z'.split())
+ARCHIVES = tuple("gz bz2 zip tar tgz txz 7z".split())
 
 #: This contains nonexecutable source files - those which need to be
-#: compiled or assembled to binaries to be used. They are generally safe to 
+#: compiled or assembled to binaries to be used. They are generally safe to
 #: accept, as without an existing RCE vulnerability, they cannot be compiled,
 #: assembled, linked, or executed. Supports C, C++, Ada, Rust, Go (Golang),
-#: FORTRAN, D, Java, C Sharp, F Sharp (compiled only), COBOL, Haskell, and 
+#: FORTRAN, D, Java, C Sharp, F Sharp (compiled only), COBOL, Haskell, and
 #: assembly.
-SOURCE = tuple(('c cpp c++ h hpp h++ cxx hxx hdl ' # C/C++
-        + 'ada ' # Ada
-        + 'rs ' # Rust
-        + 'go ' # Go
-        + 'f for f90 f95 f03 ' # FORTRAN
-        + 'd dd di ' # D
-        + 'java ' # Java
-        + 'hs ' # Haskell
-        + 'cs ' # C Sharp
-        + 'fs ' # F Sharp compiled source (NOT .fsx, which is interactive-ready)
-        + 'cbl cob ' # COBOL
-        + 'asm s ' # Assembly
-        ).split())
+SOURCE = tuple(
+    (
+        "c cpp c++ h hpp h++ cxx hxx hdl "  # C/C++
+        + "ada "  # Ada
+        + "rs "  # Rust
+        + "go "  # Go
+        + "f for f90 f95 f03 "  # FORTRAN
+        + "d dd di "  # D
+        + "java "  # Java
+        + "hs "  # Haskell
+        + "cs "  # C Sharp
+        + "fs "  # F Sharp compiled source (NOT .fsx, which is interactive-ready)
+        + "cbl cob "  # COBOL
+        + "asm s "  # Assembly
+    ).split()
+)
 
 #: This contains shared libraries and executable files (.so, .exe and .dll).
 #: Most of the time, you will not want to allow this - it's better suited for
 #: use with `AllExcept`.
-EXECUTABLES = tuple('so exe dll'.split())
+EXECUTABLES = tuple("so exe dll".split())
 
 #: The default allowed extensions - `TEXT`, `DOCUMENTS`, `DATA`, and `IMAGES`.
 DEFAULTS = TEXT + DOCUMENTS + IMAGES + DATA
@@ -99,13 +93,11 @@ def tuple_from(*iters):
 
 
 def extension(filename):
-    ext = os.path.splitext(filename)[1]
-    if ext == '':
-        # add non-ascii filename support
-        ext = os.path.splitext(filename)[0]
-    if ext.startswith('.'):
+    main, ext = os.path.splitext(filename)
+    if ext.startswith("."):
         # os.path.splitext retains . separator
         ext = ext[1:]
+
     return ext
 
 
@@ -121,19 +113,20 @@ def lowercase_ext(filename):
 
     :param filename: The filename to ensure has a lowercase extension.
     """
-    if '.' in filename:
-        main, ext = os.path.splitext(filename)
+    ext = extension(filename)
+    secured = secure_filename(filename)
+    if not ext:
+        return secured
+
+    if "." not in secured:
+        return secured + "." + ext.lower()
+    else:
+        main, ext = os.path.splitext(secured)
         return main + ext.lower()
-    # For consistency with os.path.splitext,
-    # do not treat a filename without an extension as an extension.
-    # That is, do not return filename.lower().
-    return filename
 
 
 def addslash(url):
-    if url.endswith('/'):
-        return url
-    return url + '/'
+    return url if url.endswith("/") else url + "/"
 
 
 def patch_request_class(app, size=64 * 1024 * 1024):
@@ -163,13 +156,11 @@ def patch_request_class(app, size=64 * 1024 * 1024):
                  setting will be used to patch.
     """
     if size is None:
-        if isinstance(app.request_class.__dict__['max_content_length'],
-                      property):
+        if isinstance(app.request_class.__dict__["max_content_length"], property):
             return
-        size = app.config.get('MAX_CONTENT_LENGTH')
+        size = app.config.get("MAX_CONTENT_LENGTH")
     reqclass = app.request_class
-    patched = type(reqclass.__name__, (reqclass,),
-                   {'max_content_length': size})
+    patched = type(reqclass.__name__, (reqclass,), {"max_content_length": size})
     app.request_class = patched
 
 
@@ -185,31 +176,31 @@ def config_for_set(uset, app, defaults=None):
                      settings.
     """
     config = app.config
-    prefix = 'UPLOADED_%s_' % uset.name.upper()
+    prefix = "UPLOADED_%s_" % uset.name.upper()
     using_defaults = False
     if defaults is None:
         defaults = dict(dest=None, url=None)
 
-    allow_extns = tuple(config.get(prefix + 'ALLOW', ()))
-    deny_extns = tuple(config.get(prefix + 'DENY', ()))
-    destination = config.get(prefix + 'DEST')
-    base_url = config.get(prefix + 'URL')
+    allow_extns = tuple(config.get(prefix + "ALLOW", ()))
+    deny_extns = tuple(config.get(prefix + "DENY", ()))
+    destination = config.get(prefix + "DEST")
+    base_url = config.get(prefix + "URL")
 
     if destination is None:
         # the upload set's destination wasn't given
         if uset.default_dest:
             # use the "default_dest" callable
             destination = uset.default_dest(app)
-        if destination is None: # still
+        if destination is None:  # still
             # use the default dest from the config
-            if defaults['dest'] is not None:
+            if defaults["dest"] is not None:
                 using_defaults = True
-                destination = os.path.join(defaults['dest'], uset.name)
+                destination = os.path.join(defaults["dest"], uset.name)
             else:
                 raise RuntimeError("no destination for set %s" % uset.name)
 
-    if base_url is None and using_defaults and defaults['url']:
-        base_url = addslash(defaults['url']) + uset.name + '/'
+    if base_url is None and using_defaults and defaults["url"]:
+        base_url = addslash(defaults["url"]) + uset.name + "/"
 
     return UploadConfiguration(destination, base_url, allow_extns, deny_extns)
 
@@ -231,18 +222,19 @@ def configure_uploads(app, upload_sets):
     if isinstance(upload_sets, UploadSet):
         upload_sets = (upload_sets,)
 
-    if not hasattr(app, 'upload_set_config'):
+    if not hasattr(app, "upload_set_config"):
         app.upload_set_config = {}
     set_config = app.upload_set_config
-    defaults = dict(dest=app.config.get('UPLOADS_DEFAULT_DEST'),
-                    url=app.config.get('UPLOADS_DEFAULT_URL'))
+    defaults = dict(
+        dest=app.config.get("UPLOADS_DEFAULT_DEST"), url=app.config.get("UPLOADS_DEFAULT_URL"),
+    )
 
     for uset in upload_sets:
         config = config_for_set(uset, app, defaults)
         set_config[uset.name] = config
 
     should_serve = any(s.base_url is None for s in set_config.values())
-    if '_uploads' not in app.blueprints and should_serve:
+    if "_uploads" not in app.blueprints and should_serve:
         app.register_blueprint(uploads_mod)
 
 
@@ -251,6 +243,7 @@ class All(object):
     This type can be used to allow all extensions. There is a predefined
     instance named `ALL`.
     """
+
     def __contains__(self, item):
         return True
 
@@ -272,6 +265,7 @@ class AllExcept(object):
 
         AllExcept(SCRIPTS + EXECUTABLES)
     """
+
     def __init__(self, items):
         self.items = items
 
@@ -293,6 +287,7 @@ class UploadConfiguration(object):
     :param deny: A list of extensions to deny, even if they are in the
                  `UploadSet` extensions list.
     """
+
     def __init__(self, destination, base_url=None, allow=(), deny=()):
         self.destination = destination
         self.base_url = base_url
@@ -327,7 +322,8 @@ class UploadSet(object):
                          with the app, it should return the default upload
                          destination path for that app.
     """
-    def __init__(self, name='files', extensions=DEFAULTS, default_dest=None):
+
+    def __init__(self, name="files", extensions=DEFAULTS, default_dest=None):
         if not name.isalnum():
             raise ValueError("Name must be alphanumeric (no underscores)")
         self.name = name
@@ -361,8 +357,9 @@ class UploadSet(object):
         """
         base = self.config.base_url
         if base is None:
-            return url_for('_uploads.uploaded_file', setname=self.name,
-                           filename=filename, _external=True)
+            return url_for(
+                "_uploads.uploaded_file", setname=self.name, filename=filename, _external=True,
+            )
         else:
             return base + filename
 
@@ -401,11 +398,12 @@ class UploadSet(object):
 
         :param ext: The extension to check, without the dot.
         """
-        return ((ext in self.config.allow) or
-                (ext in self.extensions and ext not in self.config.deny))
+        return (ext in self.config.allow) or (
+            ext in self.extensions and ext not in self.config.deny
+        )
 
     def get_basename(self, filename):
-        return lowercase_ext(secure_filename(filename))
+        return lowercase_ext(filename)
 
     def save(self, storage, folder=None, name=None):
         """
@@ -429,17 +427,15 @@ class UploadSet(object):
             folder, name = os.path.split(name)
 
         basename = self.get_basename(storage.filename)
-        
+
         if not self.file_allowed(storage, basename):
             raise UploadNotAllowed()
-        
+
         if name:
-            if name.endswith('.'):
+            if name.endswith("."):
                 basename = name + extension(basename)
             else:
                 basename = name
-
-
 
         if folder:
             target_folder = os.path.join(self.config.destination, folder)
@@ -474,57 +470,17 @@ class UploadSet(object):
         count = 0
         while True:
             count = count + 1
-            newname = '%s_%d%s' % (name, count, ext)
+            newname = "%s_%d%s" % (name, count, ext)
             if not os.path.exists(os.path.join(target_folder, newname)):
                 return newname
 
 
-uploads_mod = Blueprint('_uploads', __name__, url_prefix='/_uploads')
+uploads_mod = Blueprint("_uploads", __name__, url_prefix="/_uploads")
 
 
-@uploads_mod.route('/<setname>/<path:filename>')
+@uploads_mod.route("/<setname>/<path:filename>")
 def uploaded_file(setname, filename):
     config = current_app.upload_set_config.get(setname)
     if config is None:
         abort(404)
     return send_from_directory(config.destination, filename)
-
-
-class TestingFileStorage(FileStorage):
-    """
-    This is a helper for testing upload behavior in your application. You
-    can manually create it, and its save method is overloaded to set `saved`
-    to the name of the file it was saved to. All of these parameters are
-    optional, so only bother setting the ones relevant to your application.
-
-    :param stream: A stream. The default is an empty stream.
-    :param filename: The filename uploaded from the client. The default is the
-                     stream's name.
-    :param name: The name of the form field it was loaded from. The default is
-                 `None`.
-    :param content_type: The content type it was uploaded as. The default is
-                         ``application/octet-stream``.
-    :param content_length: How long it is. The default is -1.
-    :param headers: Multipart headers as a `werkzeug.Headers`. The default is
-                    `None`.
-    """
-    def __init__(self, stream=None, filename=None, name=None,
-                 content_type='application/octet-stream', content_length=-1,
-                 headers=None):
-        FileStorage.__init__(self, stream, filename, name=name,
-            content_type=content_type, content_length=content_length,
-            headers=None)
-        self.saved = None
-
-    def save(self, dst, buffer_size=16384):
-        """
-        This marks the file as saved by setting the `saved` attribute to the
-        name of the file it was saved to.
-
-        :param dst: The file to save to.
-        :param buffer_size: Ignored.
-        """
-        if isinstance(dst, string_types):
-            self.saved = dst
-        else:
-            self.saved = dst.name
